63c199fad75c0eba2330e308898a998589c46253
allysonhalley <isolation85@gmail.com>
2016-07-26 01:40:15 -0300

    tradução de mensagens de contato

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'contato@hefti.com.br'; // Add your email address inbetween the '' replacing contato@hefti.com.br - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = 'contato@hefti.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Hefti contato:  $name";
+$email_body = "Você recebeu uma nova mensagem de contato@hefti.com.br.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nFone: $phone\n\nMensagem:\n$message";
+$headers = "From: noreply@hefti.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
